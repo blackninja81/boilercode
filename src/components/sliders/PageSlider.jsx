21bad4c13fcf89aba32a1481ee97c49d1859a04d
blackninja81 <johnkaburu81@gmail.com>
2023-07-14 17:41:26 +0300

    [BUG] fixed stacking of navbar 1

---
+++ src/components/sliders/PageSlider.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react';
+import sliderStyle from '../../Styles/FullSlider.module.scss'
+
+
+const PageSlider = () => {
+  return (
+    <div>fullPageslider123</div>
+  )
+}
+
+export default PageSlider
